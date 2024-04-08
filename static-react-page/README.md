6f2574630dce7d49dd2fd01f2c1df2334191e4f5
Khrystyna Kruk <krystynakruk0@gmail.com>
2024-04-08 21:52:51 +0200

    page static styling

---
+++ static-react-page/README.md
@@ -0,0 +1,1 @@
+# Static React Page-Notes
---
+++ static-react-page/src/MyApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const MyApp = () => {
+  return (
+    <><ul>
+      <li>Hello 1</li>
+      </ul><ul>
+      <li>Hello 2</li>
+    </ul>
+    </>
+  )
+}
+
+export default MyApp
