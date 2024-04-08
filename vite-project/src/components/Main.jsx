a6abdcf889d79b4c54bb21ec864911dd161d515f
Khrystyna Kruk <krystynakruk0@gmail.com>
2024-04-08 23:05:40 +0200

    creating components

---
+++ vite-project/src/components/Main.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+export default function  Main(){
+return(
+<div>
+    <h1>Fun facts about React</h1>
+    <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+    </ul>
+</div>
+
+)
+
+
+}
