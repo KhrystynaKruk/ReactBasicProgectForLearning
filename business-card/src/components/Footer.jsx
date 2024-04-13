a3d46c178ed597ae21b9ea761afb43a6ed35eaab
Khrystyna Kruk <krystynakruk0@gmail.com>
2024-04-13 20:22:40 +0200

    card footer added

--- business-card/src/components/Footer.jsx
+++ business-card/src/components/Footer.jsx
@@ -1,5 +1,15 @@
+import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa';
+
+
 export default function Footer(){
-return(
-    <p></p>
-)
+    return (
+      
+          <div className="footer">
+            <FaInstagram className="icon" />
+            <FaFacebookF className="icon" />
+            <FaTwitter className="icon" />
+        </div>
+         
+      
+    );
 }
